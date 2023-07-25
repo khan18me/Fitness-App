6e6ed4c4b5d624375cd0125de683d714a13a7a9a
[Khan Mustufa] <[vu4s2021038@pvppcoe.ac.in]>
2023-07-25 23:46:34 +0530

    Initial Commit

--- app/src/main/java/com/example/fitness_app/diet.java
+++ app/src/main/java/com/example/fitness_app/diet.java
@@ -2,7 +2,10 @@ package com.example.fitness_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class diet extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class diet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet);
+
+
+        final Button wg_btn = (Button) findViewById(R.id.wg_btn);
+        wg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), weight_gain.class);
+                startActivity(i);
+
+
+            }
+        });
+        final Button wl_btn = (Button) findViewById(R.id.wl_btn);
+        wl_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), weight_loss.class);
+                startActivity(i);
+
+
+            }
+        });
     }
 }
