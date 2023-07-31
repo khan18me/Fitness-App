eb95bcdabca95abede5e7dff543239701c7597d4
[Khan Mustufa] <[vu4s2021038@pvppcoe.ac.in]>
2023-07-31 17:19:44 +0530

    Initial Commit

--- app/src/main/java/com/example/fitness_app/prepathome.java
+++ app/src/main/java/com/example/fitness_app/prepathome.java
@@ -21,19 +21,19 @@ public class prepathome extends AppCompatActivity {
         webView1.setWebChromeClient(new WebChromeClient());
 
         WebView webView2 = findViewById(R.id.webView2);
-        String video2 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/s1BaTeLuCzk\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+        String video2 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/BGfu6m3Mz5U\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
         webView2.loadData(video2, "text/html","utf-8");
         webView2.getSettings().setJavaScriptEnabled(true);
         webView2.setWebChromeClient(new WebChromeClient());
 
         WebView webView3 = findViewById(R.id.webView3);
-        String video3 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/s1BaTeLuCzk\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+        String video3 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/X96nUaSDmyI\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
         webView3.loadData(video3, "text/html","utf-8");
         webView3.getSettings().setJavaScriptEnabled(true);
         webView3.setWebChromeClient(new WebChromeClient());
 
         WebView webView4 = findViewById(R.id.webView4);
-        String video4 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/s1BaTeLuCzk\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+        String video4 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/I-mvBh837Wg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
         webView4.loadData(video4, "text/html","utf-8");
         webView4.getSettings().setJavaScriptEnabled(true);
         webView4.setWebChromeClient(new WebChromeClient());
